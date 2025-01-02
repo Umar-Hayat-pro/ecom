d975e877d4d239dc0de7355f12598f42ac6e9adb
Umar Hayat <umar@DESKTOP-N2UQP56.localdomain>
2025-01-02 13:21:01 +0500

    Finished te category related changes and have added active storage for image upload

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      "Cache-Control" => "public, max-age=#{2.days.to_i}"
+      "Cache-Control" => "public, max-age=#{2.days.to_i}",
     }
   else
     config.action_controller.perform_caching = false
@@ -74,5 +74,9 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 end
+Rails.application.routes.default_url_options = {
+  host: "localhost",
+  port: 3000,
+}
